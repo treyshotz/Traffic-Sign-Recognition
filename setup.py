52fdac38a30101aca2dd08049e1f071e7e1c5014
Eirik Steira <eirsteir@stud.ntnu.no>
2021-11-10 12:53:28 +0100

    Fix dataset download tag

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ import kaggle
 
 kaggle.api.authenticate()
 
-
 url = "https://www.kaggle.com/meowmeowmeowmeowmeow/gtsrb-german-traffic-sign/download"
 
 
 if not os.path.exists("kaggle/"):
     os.mkdir("kaggle/")
-    kaggle.api.dataset_download_files('GTSRB', path='kaggle/', unzip=True)
+    kaggle.api.dataset_download_files('meowmeowmeowmeowmeow/gtsrb-german-traffic-sign', path='kaggle/', unzip=True)
     print("Finished downloading dataset, extracting files")
 
 
