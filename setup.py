0875dd2b88cb825e88ea0234d8fcea19d90dbcdc
a <a>
2021-11-14 14:38:40 +0100

    Setup the test dataset differently

--- setup.py
+++ setup.py
@@ -1,15 +1,37 @@
 import os
 import kaggle
-
-kaggle.api.authenticate()
-
-url = "https://www.kaggle.com/meowmeowmeowmeowmeow/gtsrb-german-traffic-sign/download"
+import pandas as pd
 
 
-if not os.path.exists("kaggle/"):
-    os.mkdir("kaggle/")
-    kaggle.api.dataset_download_files('meowmeowmeowmeowmeow/gtsrb-german-traffic-sign', path='kaggle/', unzip=True)
+def download_dataset():
+    kaggle.api.authenticate()
+    if not os.path.exists("kaggle/"):
+        os.mkdir("kaggle/")
+    kaggle.api.dataset_download_files(
+            'meowmeowmeowmeowmeow/gtsrb-german-traffic-sign', path='kaggle/',
+            unzip=True)
     print("Finished downloading dataset, extracting files")
 
 
-print("Script finished")
+def setup_test_dataset():
+    parent_dir = "kaggle/Test/"
+    for i in range(0, 43):
+        try:
+            path = os.path.join(parent_dir, str(i)+"/")
+            os.makedirs(path)
+        except FileExistsError as e:
+            pass
+
+    print(os.listdir("kaggle/"))
+    test_data = pd.read_csv("kaggle/Test.csv")
+    path = test_data['Path']
+    class_id = test_data['ClassId']
+    for j in range(len(test_data)):
+        print(f"current pos: {j}")
+        print(f"path of file: {path[j]}")
+        filename = path[j].replace("Test/", "")
+        print(f"class_id of file: {class_id[j]}")
+        os.replace("kaggle/" +path[j], f"kaggle/Test/{class_id[j]}/"+filename)
+
+#download_dataset()
+#setup_test_dataset()
